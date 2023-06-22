b64a6393315e8c16090848356f99adcf04ff7b70
riskadwinuraini <74886545+riskadwinuraini@users.noreply.github.com>
2023-06-23 06:02:14 +0700

    updates

--- src/test/java/starter/StepDefinitions/DeleteSteps.java
+++ src/test/java/starter/StepDefinitions/DeleteSteps.java
@@ -3,21 +3,22 @@ package starter.StepDefinitions;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import net.thucydides.core.annotations.Steps;
+import starter.OfficeBuddy.DeleteOffice;
 
 public class DeleteSteps {
     @Steps
-    DeleteSteps deleteSteps;
+    DeleteOffice deleteOffice;
     @And("I click button delete office")
-    public void iClickButtonDeleteOffice() { deleteSteps.iClickButtonDeleteOffice();
+    public void iClickButtonDeleteOffice() { deleteOffice.ClickDeleteOfficeButton();
 
     }
 
     @And("I click yes of delete office")
-    public void iClickYesOfDeleteOffice() { deleteSteps.iClickYesOfDeleteOffice();
+    public void iClickYesOfDeleteOffice() { deleteOffice.ClickYesDeleteOfficeButton();
 
     }
     @And("I click back of delete office")
-    public void iClickBackOfDeleteOffice() { deleteSteps.iClickBackOfDeleteOffice();
+    public void iClickBackOfDeleteOffice() { deleteOffice.ClickBackDeleteOfficeButton();
     }
 
     @Then("I can see notif successfully delete office")
